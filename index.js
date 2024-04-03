aa87594a71452314f92a7945ec29f12af81ab220
samuel mwangi githaiga <samuelgithaiga913@gmail.com>
2024-04-03 10:03:33 +0300

    array-reduce

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+//var totalBatteries = [4+5+3+4+4+6+5]
+//console.log(totalBatteries);
+
+//function ourReduce(totalBatteries) {
+    //console.log(ourReduce);
+
+const totalBatteries = batteryBatches.reduce(function(total, batches){
+    return batches + total
+})
+console.log(batteryBatches);
+console.log(totalBatteries);
+
